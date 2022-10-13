174a595d4853cf754c67a6cbe4968922a7c71056
Ruth Ikotun <itzsinachi@gmail.com>
2022-10-13 14:02:15 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints "programming is like building a multilingual puzzle"
+* Return: Returns 0
+*/
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}

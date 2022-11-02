f79769a9aee9c3a5d022356c6a8c5e01df9d98c8
Ruth Ikotun <itzsinachi@gmail.com>
2022-11-02 10:41:24 -0700

    Finished

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,9 +3,9 @@
 /**
   * _strlen_recursion - a function that returns the length of a string
   * @s: a pointer to the length of string to return
-  * Return: the length of a string
+  * Return: the length of s
   */
 int _strlen_recursion(char *s)
 {
-	return (s ? _strlen_recursion(s + 1) + 1 : 0);
+	return (*s ? _strlen_recursion(s + 1) + 1 : 0);
 }
